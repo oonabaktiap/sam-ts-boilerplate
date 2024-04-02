61de99e49bc1e1dd74326bdb9955cfc8142b3652
bakti apriliansyah <bakti.apriliansyah@oona-insurance.co.id>
2024-04-02 14:11:15 +0700

    providing the template level sam npm script for typeorm migration, build, and test

--- base/src/migration/1712039897146-mig.mig.ts
+++ base/src/migration/1712039897146-mig.mig.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Mig1712035905342 implements MigrationInterface {
-    name = 'Mig1712035905342'
+export class Mig1712039897146 implements MigrationInterface {
+    name = 'Mig1712039897146'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "base" ADD "age" integer`);
